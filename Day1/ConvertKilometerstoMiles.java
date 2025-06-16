47681d25bc9f4ebb10d9baaeb455a3680f451228
vishalyadav67 <122170074+vishalyadav67@users.noreply.github.com>
2025-06-16 09:04:09 +0530

    Day1 assignment completed

---
+++ Day1/ConvertKilometerstoMiles.java
@@ -0,0 +1,11 @@
+package Assignment;
+import java.util.*;
+public class ConvertKilometerstoMiles {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        double km=sc.nextDouble();
+        double convermiles=(km * 0.621371);
+        System.out.println(convermiles);
+    }
+    
+}
