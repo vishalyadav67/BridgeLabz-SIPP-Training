ffa00b5ad889bf3c9d1a4c19274a79171ccae802
vishalyadav67 <122170074+vishalyadav67@users.noreply.github.com>
2025-07-15 11:09:32 +0530

    Update SelectionSortExamScore.java

--- SortingAlogorithm/src/SelectionSortExamScore.java
+++ SortingAlogorithm/src/SelectionSortExamScore.java
@@ -1,9 +1,39 @@
-
 public class SelectionSortExamScore {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void selectionSort(int[] scores) {
+        int n = scores.length;
 
-	}
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
 
+            for (int j = i + 1; j < n; j++) {
+                if (scores[j] < scores[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = scores[minIndex];
+            scores[minIndex] = scores[i];
+            scores[i] = temp;
+        }
+    }
+
+    public static void printArray(int[] scores) {
+        for (int score : scores) {
+            System.out.print(score + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] examScores = {88, 74, 92, 67, 81, 95, 78};
+
+        System.out.println("Original Exam Scores:");
+        printArray(examScores);
+
+        selectionSort(examScores);
+
+        System.out.println("Sorted Exam Scores (Ascending):");
+        printArray(examScores);
+    }
 }
