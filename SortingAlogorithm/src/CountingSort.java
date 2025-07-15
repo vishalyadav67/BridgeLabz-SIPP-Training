bf347a1e30e92821156b66fa741bbf6f3322670b
vishalyadav67 <122170074+vishalyadav67@users.noreply.github.com>
2025-07-15 11:07:57 +0530

    Update CountingSort.java

--- SortingAlogorithm/src/CountingSort.java
+++ SortingAlogorithm/src/CountingSort.java
@@ -1,9 +1,46 @@
-
 public class CountingSort {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void countingSort(int[] ages) {
+        int min = 10;
+        int max = 18;
+        int range = max - min + 1;
 
-	}
+        int[] count = new int[range];       
+        int[] output = new int[ages.length]; 
 
+        for (int age : ages) {
+            count[age - min]++;
+        }
+
+        for (int i = 1; i < range; i++) {
+            count[i] += count[i - 1];
+        }
+
+        for (int i = ages.length - 1; i >= 0; i--) {
+            int age = ages[i];
+            output[count[age - min] - 1] = age;
+            count[age - min]--;
+        }
+
+        System.arraycopy(output, 0, ages, 0, ages.length);
+    }
+
+    public static void printArray(int[] arr) {
+        for (int val : arr) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] studentAges = {12, 15, 10, 18, 12, 14, 17, 10, 13};
+
+        System.out.println("Original Student Ages:");
+        printArray(studentAges);
+
+        countingSort(studentAges);
+
+        System.out.println("Sorted Student Ages (Ascending):");
+        printArray(studentAges);
+    }
 }
